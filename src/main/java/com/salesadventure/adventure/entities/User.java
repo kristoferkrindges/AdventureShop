f67fb39d6dfc652a2afeb42f2553c82f67ec6125
Kristofer Krindges <thecontt06@gmail.com>
2023-05-17 20:58:58 -0300

    Create Order and relation in user order

--- src/main/java/com/salesadventure/adventure/entities/User.java
+++ src/main/java/com/salesadventure/adventure/entities/User.java
@@ -1,12 +1,16 @@
 package com.salesadventure.adventure.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -19,6 +23,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User(){}
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -26,6 +33,9 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+    public List<Order> getOrders() {
+        return orders;
     }
     public Long getId() {
         return id;
