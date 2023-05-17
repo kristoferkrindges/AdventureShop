a7ed4da9bef16d9fabaeda67ee921077102265f7
Kristofer Krindges <thecontt06@gmail.com>
2023-05-17 20:12:32 -0300

    Service layer, component registration and endpoint findById

--- src/main/java/com/salesadventure/adventure/repositories/UserRepository.java
+++ src/main/java/com/salesadventure/adventure/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.salesadventure.adventure.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.salesadventure.adventure.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
