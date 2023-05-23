2be169fd7b5abf09f655315e42af2e7ce4ebaf32
Kristofer Krindges <thecontt06@gmail.com>
2023-05-23 02:49:40 -0300

    Product done

--- src/main/java/com/salesadventure/adventure/entities/Product.java
+++ src/main/java/com/salesadventure/adventure/entities/Product.java
@@ -10,21 +10,61 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
     @Transient
-    private Set<Product> products = new HashSet<>();
-    
-    public Category(){}
-    public Category(Long id, String name) {
+    private Set<Category> categories = new HashSet<>();
+
+    public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Product(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getName() {
@@ -35,16 +75,10 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Set<Product> getProducts() {
-        return products;
-    }
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -52,6 +86,7 @@ public class Category implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -60,7 +95,7 @@ public class Category implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Category other = (Category) obj;
+        Product other = (Product) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
@@ -68,4 +103,7 @@ public class Category implements Serializable {
             return false;
         return true;
     }
+
+
+
 }
