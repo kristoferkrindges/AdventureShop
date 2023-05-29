645582f676daaa63b0b412b66f2899cfef857e9b
Kristofer Krindges <thecontt06@gmail.com>
2023-05-29 17:18:23 -0300

    Exception delete

--- src/main/java/com/salesadventure/adventure/services/UserService.java
+++ src/main/java/com/salesadventure/adventure/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.salesadventure.adventure.entities.User;
 import com.salesadventure.adventure.repositories.UserRepository;
+import com.salesadventure.adventure.services.exceptions.DatabaseException;
 import com.salesadventure.adventure.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -41,6 +44,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            //e.printStackTrace();
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
