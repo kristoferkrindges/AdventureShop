f6e17d159b06d1ceab177d6c81dca9f89b7fd13a
Kristofer Krindges <thecontt06@gmail.com>
2023-05-29 17:07:48 -0300

    Exceptions

---
+++ src/main/java/com/salesadventure/adventure/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.salesadventure.adventure.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
