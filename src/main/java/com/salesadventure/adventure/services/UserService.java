db065234ee9cad6205009de2bd16178c1e94d2cf
Kristofer Krindges <thecontt06@gmail.com>
2023-06-05 10:33:47 -0300

    Exception handling update

--- src/main/java/com/salesadventure/adventure/services/UserService.java
+++ src/main/java/com/salesadventure/adventure/services/UserService.java
@@ -13,6 +13,8 @@ import com.salesadventure.adventure.repositories.UserRepository;
 import com.salesadventure.adventure.services.exceptions.DatabaseException;
 import com.salesadventure.adventure.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     @Autowired
@@ -32,15 +34,20 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
+        
     }
 
     public void delete(Long id){
